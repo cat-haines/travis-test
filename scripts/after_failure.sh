7a3775694631e53be90f187e184e0cb43f631ad9
Cat Haines <cat@pebble.com>
2016-03-14 20:15:48 -0700

    test

--- scripts/after_failure.sh
+++ scripts/after_failure.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-curl https://maker.ifttt.com/trigger/build_event/with/key/$MAKER_KEY?color=$RED
+curl https://maker.ifttt.com/trigger/build_event/with/key/$MAKER_KEY?value=$RED
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-curl https://maker.ifttt.com/trigger/build_event/with/key/$MAKER_KEY?color=$YELLOW
+curl https://maker.ifttt.com/trigger/build_event/with/key/$MAKER_KEY?value1=$YELLOW
