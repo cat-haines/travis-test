88ef4c1a3297fe86754b6df84b214cbf4802008e
Cat Haines <cat@pebble.com>
2016-03-14 20:45:49 -0700

    this should work

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-curl https://maker.ifttt.com/trigger/build_event/with/key/$MAKER_KEY?value=$GREEN
+echo https://maker.ifttt.com/trigger/build_event/with/key/$MAKER_KEY?value1=$GREEN
+curl https://maker.ifttt.com/trigger/build_event/with/key/$MAKER_KEY?value1=$GREEN
